2fb9c5a492d917088d6859a78f17cd3616c2260f
Camila <caming2309@gmail.com>
2020-06-09 13:44:37 -0300

    COMPLETED media queries footer and who we are

--- src/components/whoweare/WhoWeAre.js
+++ src/components/whoweare/WhoWeAre.js
@@ -12,14 +12,13 @@ function WhoWeAre(props) {
 
                 <div className="form">
                     <h2 className="sign-in-title">This is who we are.</h2>
+                  <div className="description-wrapper">
                    <p className="whoweare-description">La casta dolor sit amet la infanta Leonor Ipsum convoca un referendum usa muletas cillum dolore el 19 de junio por qué no se calla podemos el movimiento social lorem ipsum viva la república le llena de orgullo y voluptate una nación unida enim ad minim Pablo Iglesias la reina Leticia Lorem concentración en Sol la igualdad para todos pariatur el discurso del rey su majestad el rey de adipisicing elit pide elecciones ya democracia real la izquierda singular. Pide elecciones ya la casta dolor sit amet usa muletas cillum dolore democracia real la izquierda singular el 19 de junio podemos su majestad el rey de adipisicing elit viva la república Pablo Iglesias el movimiento social lorem ipsum el discurso del rey concentración en Sol convoca un referendum la reina Leticia Lorem la infanta Leonor Ipsum la igualdad para todos pariatur una nación unida enim ad minim por qué no se calla le llena de orgullo y voluptate.
                     
 </p>
                     <p className="whoweare-description">La casta dolor sit amet se  dolor sit amet la infanta Leonor Ipsum convoca  dolor sit amet la infanta Leonor Ipsum convoca  dolor sit amet la infanta Leonor Ipsum convoca  unida enim ad minim por qué no se calla le llena de orgullo y voluptate.
-
-
-                    
                     </p>
+                    </div>
                 </div>
             </div>
         );
